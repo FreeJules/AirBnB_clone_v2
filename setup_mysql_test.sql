7edba5107965369fc17be549e2b30cc4be9da385
Julija Lee <lee.julija@gmail.com>
2017-07-26 16:05:55 +0000

    adding 4

---
+++ setup_mysql_test.sql
@@ -0,0 +1,9 @@
+-- script that creates the database hbtn_0d_2 and the user user_0d_2
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+GRANT ALL PRIVILEGES ON hbnb_test_db.*
+TO 'hbnb_test'@'localhost'
+IDENTIFIED BY 'hbnb_test_pwd';
+GRANT SELECT ON performance_schema.*
+TO 'hbnb_test'@'localhost'
+IDENTIFIED BY 'hbnb_test_pwd';
+FLUSH PRIVILEGES;

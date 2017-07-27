5bb72ae14926435777d7179523b7f5af37ddd787
Julija Lee <lee.julija@gmail.com>
2017-07-27 06:34:26 +0000

    fixing save in base_model and relationships in place and amenity

--- models/place.py
+++ models/place.py
@@ -33,7 +33,8 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, default=0, nullable=False)
         latitude = Column(Float)
         longitude = Column(Float)
-        amenities = relationship('Amenity', secondary=place_amenity, viewonly=False)
+        amenities = relationship('Amenity', secondary="place_amenity",
+                                 viewonly=False)
         reviews = relationship('Review', backref='place')
 
     else:

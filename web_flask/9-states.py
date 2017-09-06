4b4d700ac3dc974b68cb0724e6dd160afbbe94ee
Julija Lee <lee.julija@gmail.com>
2017-09-06 16:58:05 +0000

    adding 10

---
+++ web_flask/9-states.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+from models import storage
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """calls close method of storage"""
+    storage.close()
+
+
+@app.route('/states')
+def states():
+    """states method"""
+    states = storage.all("State")
+    return render_template('7-states_list.html', states=states)
+
+
+@app.route('/states/<id>')
+def state_id(id):
+    """state_id method"""
+    try:
+        state_id = str(id)
+        states = storage.all("State").values()
+        for state in states:
+            if state.id == state_id:
+                state_name = state.name
+                cities = state.cities
+                return render_template('9-states.html', condition='found',
+                                       state_name=state_name, cities=cities)
+    except:
+        return render_template('9-states.html', condition='not_found')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')

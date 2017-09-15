149ded53c45e37f7f5f32687d34d4b21eb120ada
Julija Lee <lee.julija@gmail.com>
2017-09-15 15:30:36 +0000

    had cities outside of else statement

--- models/state.py
+++ models/state.py
@@ -17,18 +17,15 @@ class State(BaseModel, Base):
         cities = relationship('City', backref='state', cascade="all")
     else:
         name = ''
-
-    def __init__(self, *args, **kwargs):
-        """instantiates a new state"""
-        super().__init__(self, *args, **kwargs)
-
-    if environ.get("HBNB_TYPE_STORAGE") is None or \
-       getenv("HBNB_TYPE_STORAGE") != 'db':
         def cities(self):
             from models import storage
             cities_dict = storage.all("City")
             cities = []
             for item in cities_dict.values():
                 if item['state_id'] == self.id:
-                    cities.append(item)
+                                cities.append(item)
             return cities
+
+    def __init__(self, *args, **kwargs):
+        """instantiates a new state"""
+        super().__init__(self, *args, **kwargs)

1f8eada4f7a7aa76a721d481acdf3ea199c456fb
Julija Lee <lee.julija@gmail.com>
2017-09-15 18:46:18 +0000

    ading whole new one

--- models/state.py
+++ models/state.py
@@ -2,30 +2,33 @@
 """
 State Class from Models Module
 """
-from sqlalchemy import Column, String
-from sqlalchemy import *
-from sqlalchemy.orm import *
+
 from models.base_model import BaseModel, Base
-from os import getenv, environ
+import models
+from sqlalchemy import String, Column
+from sqlalchemy.orm import relationship
+from os import environ, getenv
 
 
 class State(BaseModel, Base):
     """State class handles all application states"""
-    if 'HBNB_TYPE_STORAGE' in environ and getenv("HBNB_TYPE_STORAGE") == 'db':
+
+    if 'HBNB_TYPE_STORAGE' in environ and environ['HBNB_TYPE_STORAGE'] == 'db':
+
         __tablename__ = 'states'
         name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state', cascade="all")
+        cities = relationship("City", cascade="all, delete-orphan",
+                              backref="state")
     else:
-        name = ''
+        name = ""
 
         @property
         def cities(self):
-            """
-                getter method, returns list of City objs from storage
-                linked to the current State
-            """
-            cities_list = []
-            for city in models.storage.all("City").values():
-                if city.state_id == self.id:
-                    cities_list.append(city)
-            return cities_list
+            """ returns all city objects  associated with this State  """
+            cities = models.storage.all('City').values()
+            results = [city for city in cities if city.state_id == self.id]
+            return (results)
+
+    def __init__(self, *args, **kwargs):
+        """instantiates a new state"""
+        super().__init__(self, *args, **kwargs)

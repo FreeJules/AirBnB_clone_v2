5d523aff8a0ffdf546a855bcb1d2422d5ed11579
Julija Lee <lee.julija@gmail.com>
2017-09-15 19:22:00 +0000

    reverted to old version and cleaned up imports order

--- models/state.py
+++ models/state.py
@@ -2,16 +2,15 @@
 """
 State Class from Models Module
 """
-from sqlalchemy import Column, String
-from sqlalchemy import *
-from sqlalchemy.orm import *
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 from os import environ, getenv
 
 
 class State(BaseModel, Base):
     """State class handles all application states"""
-    if 'HBNB_TYPE_STORAGE' in environ and getenv("HBNB_TYPE_STORAGE") == 'db':
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
         __tablename__ = 'states'
         name = Column(String(128), nullable=False)
         cities = relationship('City', backref='state', cascade="all")

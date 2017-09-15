32ab1bdc1d6cdc04800de8ca67e93bb41d6e0b89
Julija Lee <lee.julija@gmail.com>
2017-09-15 18:37:34 +0000

    added another if statement

--- models/state.py
+++ models/state.py
@@ -11,7 +11,7 @@ from os import getenv, environ
 
 class State(BaseModel, Base):
     """State class handles all application states"""
-    if getenv("HBNB_TYPE_STORAGE") == 'db':
+    if 'HBNB_TYPE_STORAGE' in environ and getenv("HBNB_TYPE_STORAGE") == 'db':
         __tablename__ = 'states'
         name = Column(String(128), nullable=False)
         cities = relationship('City', backref='state', cascade="all")
